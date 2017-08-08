51f50b0ca9face1869fdcf2826803683a5cc8ae5
radomir@mazon.pl <radomir@mazon.pl>
2017-08-08 13:30:13 +0200

    next

--- podlewaczka/podlewaczka.ino
+++ podlewaczka/podlewaczka.ino
@@ -17,67 +17,36 @@
 #include "RainController.h"
 #include "ButtonInput.h"
 #include "PRInput.h"
+#include "ExecutorsLogic.h"
+#include "TimerLogic.h"
 
 
 //// core objects:
+ExecutorsLogic* pExecutorsLogic;
 MainConfiguration mainConfig;
 PRInput pr;
-
-Configuration config_0;
-Configuration config_1;
-Configuration config_2;
-Configuration config_3;
-Configuration config_4;
-Configuration config_5;
-Configuration config_6;
-Configuration config_7;
-
-Executor* executor_0 = new Executor(&config_0, &pr);
-Executor* executor_1 = new Executor(&config_1, &pr);
-Executor* executor_2 = new Executor(&config_2, &pr);
-Executor* executor_3 = new Executor(&config_3, &pr);
-Executor* executor_4 = new Executor(&config_4, &pr);
-Executor* executor_5 = new Executor(&config_5, &pr);
-Executor* executor_6 = new Executor(&config_6, &pr);
-Executor* executor_7 = new Executor(&config_7, &pr);
-
-Executor* executors[8];
-
 ButtonInput button;
-
-Display* pDisplay = new Display(17, 11, 18, 12, 19, 16, 15, 13);
-RainController* pRainController = new RainController(&mainConfig);
-NightController* pNightController = new NightController(&mainConfig);
+Display* pDisplay;
+RainController* pRainController;
+NightController* pNightController;
+TimerLogic timerLogic;
 
 void setup() {
   Serial.begin(9600);  //Begin serial communcation
-  executors[0] = executor_0;
-  executors[1] = executor_1;
-  executors[2] = executor_2;
-  executors[3] = executor_3;
-  executors[4] = executor_4;
-  executors[5] = executor_5;
-  executors[6] = executor_6;
-  executors[7] = executor_7;
-  //pitout
-  config_0.setPin(2);
-  config_1.setPin(3);
-  config_2.setPin(4);
-  config_3.setPin(5);
-  config_4.setPin(6);
-  config_5.setPin(7);
-  config_6.setPin(8);
-  config_7.setPin(9);
 
-  loadConfiguration();
+  pDisplay = new Display(17, 11, 18, 12, 19, 16, 15, 13);
+  pRainController = new RainController(&mainConfig);
+  pNightController = new NightController(&mainConfig);
+
+  pExecutorsLogic = new ExecutorsLogic(&mainConfig, &pr);
+  pExecutorsLogic->loadConfiguration();
 }
 
 void oneSecondTick() {
-  for (uint8_t i=0; i<8; i++) {
-    executors[i]->tick();
-  }
   pRainController->tick();
   pNightController->tick();
+  pExecutorsLogic->tick();
+  timerLogic.tick();
 }
 
 void fastTick() {
@@ -108,18 +77,6 @@ void fastTick() {
   }
 
   pDisplay->tick();
-}
-
-void loadConfiguration() {
-   //uint8_t runWitch, boolean enabled, uint8_t fill, boolean simulateLP
-  config_0.load(RUN_INDEPENDENTLY, true, 80, false);
-  config_1.load(RUN_INDEPENDENTLY, true, 80, false);
-  config_2.load(RUN_WITCH_CH1, true, 80, false);
-  config_3.load(RUN_INDEPENDENTLY, true, 100, false);
-  config_4.load(RUN_INDEPENDENTLY, true, 100, false);
-  config_5.load(RUN_INDEPENDENTLY, false, 100, false);
-  config_6.load(RUN_INDEPENDENTLY, false, 100, false);
-  config_7.load(RUN_INDEPENDENTLY, false, 100, false);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
